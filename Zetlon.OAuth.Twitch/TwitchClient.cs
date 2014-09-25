f3328a08459469a28c1bfa9a401dfe2c6e30a921
Simon Rasmussen <zetlon@gmail.com>
2014-09-25 04:55:55 +0200

    Added Twitch OAuth client

---
+++ Zetlon.OAuth.Twitch/TwitchClient.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using DotNetOpenAuth.AspNet.Clients;
+using Newtonsoft.Json.Linq;
+
+namespace Zetlon.OAuth.Twitch
+{
+    /// <summary>
+    /// OAuth2 Client for Twitch
+    /// See more details at https://github.com/justintv/Twitch-API/blob/master/authentication.md
+    /// </summary>
+    /// <example>
+    /// OAuthWebSecurity.RegisterClient(new TwitchClient("ClientId", "ClientSecret", "channel_check_subscription"));
+    /// </example>
+    public class TwitchClient : OAuth2Client
+    {
+        private const string ApiUrl = "https://api.twitch.tv/kraken/";
+        private const string Enduserauthlink = ApiUrl + "oauth2/authorize?response_type=code";
+        private const string TokenLink = ApiUrl + "oauth2/token?&grant_type=authorization_code";
+        private const string UserUrl = ApiUrl + "user/";
+
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _scopes;
+
+        /// <summary>
+        /// Dictionary containing user data keys that must be renamed to fit OAuth2Client naming
+        /// </summary>
+        private static readonly Dictionary<string, string> Replacements = new Dictionary<string, string>()
+        {
+            {"name", "username"},
+            {"_id", "id"}
+        }; 
+        
+        public TwitchClient(string clientId, string clientSecret, IEnumerable<string> scopes) : base("Twitch")
+        {
+            if (clientId == null) throw new ArgumentNullException("clientId");
+            if (clientSecret == null) throw new ArgumentNullException("clientSecret");
+            if (scopes == null) throw new ArgumentNullException("scopes");
+            
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _scopes = CreateScopeString(scopes);
+        }
+
+        public TwitchClient(string clientId, string clientSecret, params string[] scopes)
+            : this(clientId, clientSecret, (IEnumerable<string>) scopes){ }
+
+        protected override Uri GetServiceLoginUrl(Uri returnUrl)
+        {
+            var b = new StringBuilder();
+
+            b.Append(Enduserauthlink);
+            AppendDefaults(b, returnUrl);
+            b.Append("&scope=");
+            b.Append(_scopes);
+            
+            return new Uri(b.ToString());
+        }
+
+        protected override string QueryAccessToken(Uri returnUrl, string authorizationCode)
+        {
+            var b = new StringBuilder();
+
+            b.Append(TokenLink);
+            AppendDefaults(b, returnUrl);
+            b.AppendFormat("&client_secret={0}", _clientSecret);
+            b.AppendFormat("&code={0}", authorizationCode);
+            
+            using (var client = new WebClient())
+            {
+                SetHeaders(client);
+
+                var response = client.UploadString(b.ToString(), "POST", "");
+
+                var json = JObject.Parse(response);
+
+                return json.GetValue("access_token").ToObject<string>();
+            }
+        }
+
+        protected override IDictionary<string, string> GetUserData(string accessToken)
+        {
+            using (var client = new WebClient())
+            {
+                SetHeaders(client);
+
+                var response = client.UploadString(UserUrl, "POST", "");
+
+                var json = JObject.Parse(response);
+
+                var dic = new Dictionary<string, string>();
+
+                foreach (var pair in json)
+                {
+                    var name = Replacements.ContainsKey(pair.Key) ? Replacements[pair.Key] : pair.Key;
+                    var value = pair.Value.ToString();
+
+                    dic.Add(name, value);
+                }
+
+                return dic;
+            }
+        }
+
+        /// <summary>
+        /// Appends client id and return url to a string builder
+        /// </summary>
+        private void AppendDefaults(StringBuilder b, Uri returnUrl)
+        {
+            b.AppendFormat("&client_id={0}", _clientId);
+            b.AppendFormat("&redirect_uri={0}", Uri.EscapeUriString(returnUrl.ToString()));
+        }
+
+        /// <summary>
+        /// Sets the header of a WebClient instance to api version 3 and provides the client id to avoid getting rate limited.
+        /// The auth token is also set if provided.
+        /// </summary>
+        private void SetHeaders(WebClient client, string token = null)
+        {
+            var headers = new WebHeaderCollection
+            {
+                {HttpRequestHeader.Accept, "application/vnd.twitchtv.v3+json"},
+                {"Client-ID", _clientId}
+            };
+
+            if(!string.IsNullOrWhiteSpace(token))
+                headers.Add(HttpRequestHeader.Authorization, string.Format("OAuth {0}", token));
+
+            client.Headers = headers;
+        }
+
+        /// <summary>
+        /// Returns a scope string formatted acording to twitch rules, ex: "user_read+channel_check_subscription"
+        /// </summary>
+        private static string CreateScopeString(IEnumerable<string> scopes)
+        {
+            //user_read scope is required as a minimum for GetUserData
+            var set = new HashSet<string>(scopes, StringComparer.OrdinalIgnoreCase) { "user_read" };
+            var b = new StringBuilder();
+
+            string prefix = "";
+            foreach (var scope in set)
+            {
+                b.Append(prefix);
+                b.Append(scope);
+                prefix = "+";
+            }
+
+            return b.ToString();
+        }
+    }
+}
